75c7c9ee59183a81f9f9672e82c7e6e0ed99c9df
Никита Нестерук <nikitanesteruk@MacBook-Air-Nikita.local>
2025-02-19 17:37:32 +0300

    created a new hepler in crypta-card

---
+++ src/entities/crypta-card/helpers/format-cell.helpers.tsx
@@ -0,0 +1,32 @@
+export const formatCell = (key: string, value: string) => {
+  if (!value) return "-";
+  if (key === "changePercent24Hr") {
+    const numValue = Number(value);
+    return (
+      <span className={numValue > 0 ? "green" : "red"}>
+        {numValue.toFixed(2)} %
+      </span>
+    );
+  }
+  if (key === "explorer") {
+    return (
+      <a href={String(value)} target="_blank" className="card__link">
+        {value.slice(0, 30)}
+      </a>
+    );
+  }
+  const numValue = typeof value === "string" ? Number(value) : value;
+  if (isNaN(numValue)) return value;
+
+  switch (true) {
+    case numValue >= 1e9:
+      return `${(numValue / 1e9).toFixed(2)} млрд $`;
+    case numValue >= 1e6:
+      return `${(numValue / 1e6).toFixed(2)} млн $`;
+    case numValue >= 1e3:
+      return `${numValue.toFixed(2)} $`;
+
+    default:
+      return `${numValue.toFixed(2).toString()} $`;
+  }
+};
--- src/entities/crypta-card/ui/crypta-card.component.tsx
+++ src/entities/crypta-card/ui/crypta-card.component.tsx
@@ -3,6 +3,7 @@ import { BasicTable } from "shared/ui/table";
 import { useStateLocation } from "shared/hooks";
 import { CryptaInRow } from "../model/crypta-card.types";
 import "./crypta-card.styles.scss";
+import { formatCell } from "../helpers/format-cell.helpers";
 
 export const CryptaCard = () => {
   const { currency } = useStateLocation();
@@ -15,39 +16,6 @@ export const CryptaCard = () => {
     vwap24Hr: "Средняя цена по объёму за последние 24 часа",
     changePercent24Hr: "Процентные изменения цены за последние 24 часа",
     explorer: "Сайт",
-  };
-
-  const formatCell = (key: string, value: string) => {
-    if (!value) return "-";
-    if (key === "changePercent24Hr") {
-      const numValue = Number(value);
-      return (
-        <span className={numValue > 0 ? "green" : "red"}>
-          {numValue.toFixed(2)} %
-        </span>
-      );
-    }
-    if (key === "explorer") {
-      return (
-        <a href={String(value)} target="_blank" className="card__link">
-          {value.slice(0, 30)}
-        </a>
-      );
-    }
-    const numValue = typeof value === "string" ? Number(value) : value;
-    if (isNaN(numValue)) return value;
-
-    switch (true) {
-      case numValue >= 1e9:
-        return `${(numValue / 1e9).toFixed(2)} млрд $`;
-      case numValue >= 1e6:
-        return `${(numValue / 1e6).toFixed(2)} млн $`;
-      case numValue >= 1e3:
-        return `${numValue.toFixed(2)} $`;
-
-      default:
-        return `${numValue.toFixed(2).toString()} $`;
-    }
   };
 
   const tableData: CryptaInRow[] = Object.entries(currency)
